adaa2f5f55c46acb0fbd718640271e401551ac8a
msv <152381355+eslopi@users.noreply.github.com>
2024-10-05 16:33:06 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const socketIo = require('socket.io');
 require('dotenv').config();
 
 const app = express();
